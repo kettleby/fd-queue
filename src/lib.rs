d5adc6adf70ffde4aa004d1b9b4054e1030c625b
Steven Bosnick <sbosnick@sympatico.ca>
2020-05-31 22:31:06 -0400

    Add EnqueueFd and DequeueFd traits
    These traits are the central abstraction of this crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,60 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// Copyright 2020 Steven Bosnick
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE-2.0 or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms
+
+//! A library for provinding abstractions for passing [`RawFd`][RawFd] between
+//! processes. This is necessarily a Unix (or Unix-like) library as `RawFd` are Unix
+//! specific.
+//!
+//! The underlying mechanism for passing the `RawFd` is a Unix socket, but the
+//! different abstractions provided here are different ways of embedding this in the
+//! Rust ecosystem.
+//!
+//! [RawFd]: https://doc.rust-lang.org/stable/std/os/unix/io/type.RawFd.html
+
+#![deny(missing_docs, warnings)]
+
+use std::os::unix::io::{AsRawFd, RawFd};
+
+/// An interface to enqueue a [`RawFd`][RawFd] for later tranmission to a different
+/// process.
+///
+/// This trait is intended to interact with [`Write`][Write] as the mechanism for
+/// actually transmitting the enqueued [`RawFd`][RawFd]. Specfically, the `RawFd`
+/// will be transmittied after a `write()` of at least 1 byte and, possibly, a
+/// `flush()`.
+///
+/// [RawFd]: https://doc.rust-lang.org/stable/std/os/unix/io/type.RawFd.html
+/// [Write]: https://doc.rust-lang.org/stable/std/io/trait.Write.html
+pub trait EnqueueFd {
+
+    /// Enqueue `fd` for later tranmission to a different process.
+    ///
+    /// The caller is responsible for keeping `fd` open until after the `write()` and
+    /// `flush()` calls for actually transmitting the `fd` have been completed.
+    fn enqueue(fd: impl AsRawFd);
+}
+
+/// An interface to dequeue a [`RawFd`][RawFd] that was previously transmitted from a
+/// different process.
+///
+/// This trait is intended to interact with [`Read`][Read] as the mechanism for
+/// actually tranmitting the [`RawFd`][RawFd] before it can be dequeued. Specfically
+/// the `RawFd` will become available for dequeuing after at least 1 byte has been
+/// `read()`.
+///
+/// [RawFd]: https://doc.rust-lang.org/stable/std/os/unix/io/type.RawFd.html
+/// [Read]: https://doc.rust-lang.org/stable/std/io/trait.Read.html
+pub trait DequeueFd {
+
+    /// Dequeue a previouly transmitted [`RawFd`][RawFd].
+    ///
+    /// The caller is responsible for closing this `RawFd`.
+    ///
+    /// [RawFd]: https://doc.rust-lang.org/stable/std/os/unix/io/type.RawFd.html
+    fn dequeue() -> RawFd;
 }
