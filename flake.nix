67dbf5f73cefabfa050328740973f9df0e91bd6e
Steven Bosnick <sbosnick@sympatico.ca>
2022-07-17 22:44:38 -0400

    chore(devenv): add release-plz to the devenv

--- flake.nix
+++ flake.nix
@@ -3,24 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-apps.url = "github:sbosnick/rust-apps";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, rust-apps }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default ];
+          overlays = [
+            rust-overlay.overlays.default
+            rust-apps.overlays.default
+          ];
         };
     in
     {
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = [
           pkgs.rust-bin.stable.latest.default
+          pkgs.rust-apps.release-plz
           pkgs.cargo-edit
           pkgs.cargo-outdated
         ];
